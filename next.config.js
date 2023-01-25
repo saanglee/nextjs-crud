d17f3ffbafe7abd97092568f4bec484fbd223459
Sangji <sjlee@beyless.com>
2023-01-25 19:16:11 +0900

    setting files

---
+++ next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/dummyapi.io/data/api/',
+        destination: '/test',
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig

d7958922bd8ced3a96b5efa8f587bd4ce87953f3
Sangji <sjlee@beyless.com>
2023-01-27 18:06:04 +0900

    fix: apikey env변수로 수정

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -2,8 +2,8 @@ import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDjFy4PGbLxPVE6dYHzFimyyoPm7MCr9jU',
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,

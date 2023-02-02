7df45399cc6c3d55bc88197f9b81ffe21a18a17d
Sangji <sjlee@beyless.com>
2023-02-02 14:04:44 +0900

    feature: 모바일용 헤더 기능 추가

---
+++ src/components/layout/MobileHeader.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './MobileHeader.module.scss';
+import Link from 'next/link';
+import { LoginOutlined, UserOutlined } from '@ant-design/icons';
+import { auth } from '../../firebase/firebaseClient';
+import { useIdToken } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
+import { useRouter } from 'next/router';
+
+const cx = classNames.bind(styles);
+const iconStyle = {
+  fontSize: 24,
+  paddingRight: 10,
+  paddingLeft: 10,
+};
+
+const MobileHeader = () => {
+  const router = useRouter();
+  const [user] = useIdToken(auth);
+
+  const handleLogoutClick = () => {
+    signOut(auth).then(() => {
+      alert('로그아웃!');
+    });
+    router.push('/login');
+  };
+
+  return (
+    <div className={cx('mobile-header')}>
+      <div className={cx('logo')}>
+        <Link href="/" className="logo__text">
+          React CRUD
+        </Link>
+      </div>
+      <div className="mobile-header__auth">
+        {user ? (
+          <>
+            <div className={cx('profile')}>
+              <UserOutlined style={iconStyle} />
+              <span className="profile__name">{user?.email?.split('@')[0]}</span>
+              <span>님</span>
+            </div>
+            <button className={cx('auth-btn')} onClick={handleLogoutClick}>
+              <span className="auth-btn__text">LOG OUT</span>
+              <LoginOutlined style={iconStyle} />
+            </button>
+          </>
+        ) : (
+          <button className={cx('auth-btn')} onClick={() => router.push('/login')}>
+            <span className="auth-btn__text">LOG IN</span>
+            <LoginOutlined style={iconStyle} />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MobileHeader;

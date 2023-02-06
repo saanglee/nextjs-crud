5335c7432bd1324b28476cb195290a9c89474dbe
Sangji <sjlee@beyless.com>
2023-02-06 13:46:19 +0900

    feat: 오픈 그래프 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/layout';
 
 import '../styles/index.scss';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 interface AppProps {
   Component: FunctionComponent;
@@ -14,12 +15,24 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
   const user = useAuth();
   if (!user) router.replace('/login');
+  const pathName = router.asPath.replace('/', '');
+
   return (
-    <AuthProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthProvider>
+    <>
+      <Head>
+        <meta name="referrer" content="unsafe-url" />
+        <meta name="description" content="CRUD project with react, next.js, & firebase" />
+        <meta property="og:imgae" content="../assets/images/thumbnail.png" />
+        <meta property="og:site_name" content="React CRUD" />
+        <meta property="og:description" content="CRUD project with react & next.js" />
+        <title> {pathName && `${pathName} • `}ReactCRUD</title>
+      </Head>
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
+    </>
   );
 };
 

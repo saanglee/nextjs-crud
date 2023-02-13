74c1d0cc7aa71c87c7ec3a2696719fd28b6f936d
Sangji <sjlee@beyless.com>
2023-02-13 18:26:38 +0900

    modify: 미인증 사용자 리다이렉트방법 수정

--- src/pages/new-post/index.tsx
+++ src/pages/new-post/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import NewPost, { PostForm } from '../../components/posts/NewPost';
 import { useRouter } from 'next/router';
-import { useAuth } from 'context/authProvider';
 import Message from 'components/shared/Message';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase/firebaseClient';
 
 const NewPostPage = () => {
-  const { user } = useAuth();
   const router = useRouter();
+  const [user, loading] = useAuthState(auth);
   const uid = user?.uid as string;
+
+  if (loading) {
+    return <div>로딩 중...</div>;
+  }
 
   const addPostHandler = async (newPost: PostForm): Promise<any> => {
     try {
@@ -30,7 +35,7 @@ const NewPostPage = () => {
 
   if (!user)
     return (
-      <Message messageTitle="로그인 이후에 이용하실 수 있습니다." buttonText="로그인 페이지로 가기" linkTo="/login" />
+      <Message messageTitle="로그인 이후에 이용하실 수 있습니다." buttonText="로그인 페이지로 가기 →" linkTo="/login" />
     );
   return <NewPost addPostHandler={addPostHandler} />;
 };
